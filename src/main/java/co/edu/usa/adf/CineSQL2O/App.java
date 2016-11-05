14e5be999d4765e86f97d7035689a1f49c531a68
Mario Ortega <root@bellyoz-pc.bellyoz.com>
2016-11-04 20:17:36 -0500

    add change one

---
+++ src/main/java/co/edu/usa/adf/CineSQL2O/App.java
@@ -0,0 +1,13 @@
+package co.edu.usa.adf.CineSQL2O;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola mundo" );
+    }
+}
